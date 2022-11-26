820ff704ea61b907f155764d1c0ba70a291fcb2c
sedmacus <98165455+sedmacus@users.noreply.github.com>
2022-11-26 02:38:54 +0100

    Add files via upload

---
+++ Appp/Branch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appp
+{
+    public class Branch
+    {
+        public List<Branch> branches { get; set; }
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+
+
+        void printTree(Branch branch)
+        {
+            if (branch.branches.Count == 0)
+            {
+                Console.WriteLine("\n");
+            }
+            branch.branches.ForEach(x => Console.WriteLine(" O "));
+            branch.branches.ForEach(x => printTree(x));
+        }
+    }
+}
---
+++ Appp/BranchService.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Appp
+{
+    public static class BranchService
+    {
+        public static int getDepthOfTree(Branch tree)
+        {
+            if (tree.branches.Count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return tree.branches.Select(x => 1 + getDepthOfTree(x)).Max();
+            }
+        }
+    }
+}
